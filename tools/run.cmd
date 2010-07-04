2db27518aff47ccefee75d590d5c9a6169618787
78 <78@nreez.com>
2010-07-05 02:38:32 +0300

    Add autobuild scripts.

--- tools/run.cmd
+++ tools/run.cmd
@@ -4,5 +4,5 @@ rem -- Run tests with msbuild --
 
 rem More info - http://xunit.codeplex.com/Wiki/View.aspx?title=HowToUseMSBuild
 
-c:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe run.targets
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe run.targets
 pause
