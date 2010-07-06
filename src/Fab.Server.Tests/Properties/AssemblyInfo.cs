e46777000fded54a3d431e8ec74fa0a2c48716b7
Sevenate <1175845+sevenate@users.noreply.github.com>
2010-07-06 04:24:44 +0300

    Add version number autogeneration.

--- src/Fab.Server.Tests/Properties/AssemblyInfo.cs
+++ src/Fab.Server.Tests/Properties/AssemblyInfo.cs
@@ -1,14 +1,7 @@
-﻿// <copyright file="AssemblyInfo.cs" company="HD">
-// 	Copyright (c) 2010 HD. All rights reserved.
+﻿// <copyright file="AssemblyInfo.cs" company="nReez">
+//  Copyright (c) 2009-2010 nReez. All rights reserved.
 // </copyright>
-// <author name="Andrew Levshoff">
-// 	<email>alevshoff@hd.com</email>
-// 	<date>2010-01-30</date>
-// </author>
-// <editor name="Andrew Levshoff">
-// 	<email>alevshoff@hd.com</email>
-// 	<date>2010-01-30</date>
-// </editor>
+// <author name="Andrew Levshoff" email="78@nreez.com" date="2010-01-30" />
 // <summary>Assembly information.</summary>
 
 using System.Reflection;
@@ -18,31 +11,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fab.Server.Tests")]
-[assembly: AssemblyDescription("FAB WCF Service Unit Tests")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HD")]
-[assembly: AssemblyProduct("FAB")]
-[assembly: AssemblyCopyright("Copyright © HD 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("FAB WCF Service xUnit Tests")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("29e60332-7f1e-4419-bc63-e4416dd635a5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Fab.Server/Properties/AssemblyInfo.cs
+++ src/Fab.Server/Properties/AssemblyInfo.cs
@@ -1,14 +1,7 @@
-﻿// <copyright file="AssemblyInfo.cs" company="HD">
-// 	Copyright (c) 2010 HD. All rights reserved.
+﻿// <copyright file="AssemblyInfo.cs" company="nReez">
+//  Copyright (c) 2009-2010 nReez. All rights reserved.
 // </copyright>
-// <author name="Andrew Levshoff">
-// 	<email>alevshoff@hd.com</email>
-// 	<date>2010-01-30</date>
-// </author>
-// <editor name="Andrew Levshoff">
-// 	<email>alevshoff@hd.com</email>
-// 	<date>2010-01-30</date>
-// </editor>
+// <author name="Andrew Levshoff" email="78@nreez.com" date="2010-01-30" />
 // <summary>Assembly information.</summary>
 
 using System.Reflection;
@@ -20,33 +13,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Fab.Server")]
 [assembly: AssemblyDescription("FAB WCF Service")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HD")]
-[assembly: AssemblyProduct("FAB")]
-[assembly: AssemblyCopyright("Copyright © HD 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bff51f58-6e89-4d0b-9695-701029d0d4e3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Allow EmitMapper access to anonymous types
 [assembly: InternalsVisibleTo("EmitMapperAssembly")]
