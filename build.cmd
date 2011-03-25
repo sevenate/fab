f3c1d3ddb32cac307e00c5632d5c58fae03c8f10
78 <78@nreez.com>
2011-03-25 03:13:09 +0200

    Moved start page (index.html) from Fab.Client.Web to Fab.Server; Removed Fab.Client.Web project.

--- build.cmd
+++ build.cmd
@@ -1,10 +1,9 @@
 @echo off
 
 rem -- Prevent creating "Fab.sln.cache" file --
-rem set MSBuildUseNoSolutionCache=1
+set MSBuildUseNoSolutionCache=1
 
-rem C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:Clean /p:Configuration=Release Fab.sln
-rem C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:ReBuild /p:Configuration=Release Fab.sln
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:Clean /p:Configuration=Release Fab.sln
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:ReBuild /p:Configuration=Release Fab.sln
 
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Fab.sln /t:ReBuild /p:Configuration=Release
 pause
--- src/Fab.Client.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fab.Client.Web.MvcApplication" Language="C#" %>
