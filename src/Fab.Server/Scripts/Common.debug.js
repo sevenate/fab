fddd480cd6dcb9f940e9a58b6aad82a8f9ae241e
Sevenate <1175845+sevenate@users.noreply.github.com>
2012-06-06 02:14:24 +0300

    Added custom search engine list to Google Analytics.

---
+++ src/Fab.Server/Scripts/Common.debug.js
@@ -0,0 +1,43 @@
+﻿function addCustomSEToGA()
+{
+	try
+	{
+		pageTracker._addOrganic("rambler.ru", "words");
+		pageTracker._addOrganic("nova.rambler.ru", "query");
+		pageTracker._addOrganic("mail.ru", "q");
+		pageTracker._addOrganic("go.mail.ru", "q");
+		pageTracker._addOrganic("search.otvet.mail.ru", "q");
+		pageTracker._addOrganic("aport.ru", "r");
+		pageTracker._addOrganic("metabot.ru", "st");
+		pageTracker._addOrganic("meta.ua", "q");
+		pageTracker._addOrganic("bigmir.net", "q");
+		pageTracker._addOrganic("nigma.ru", "s");
+		pageTracker._addOrganic("search.ukr.net", "search_query");
+		pageTracker._addOrganic("start.qip.ru", "query");
+		pageTracker._addOrganic("gogle.com.ua", "q");
+		pageTracker._addOrganic("google.com.ua", "q");
+		pageTracker._addOrganic("images.google.com.ua", "q");
+		pageTracker._addOrganic("search.winamp.com", "query");
+		pageTracker._addOrganic("search.icq.com", "q");
+		pageTracker._addOrganic("m.yandex.ru", "query");
+		pageTracker._addOrganic("gde.ru", "keywords");
+		pageTracker._addOrganic("genon.ru", "QuestionText");
+		
+		pageTracker._addOrganic("blogs.yandex.ru", "text");
+		pageTracker._addOrganic("webalta.ru", "q");
+		pageTracker._addOrganic("akavita.by", "z");
+		pageTracker._addOrganic("tut.by", "query");
+		pageTracker._addOrganic("all.by", "query");
+		pageTracker._addOrganic("i.ua", "q");
+		pageTracker._addOrganic("online.ua", "q");
+		pageTracker._addOrganic("a.ua", "s");
+		pageTracker._addOrganic("ukr.net", "search_query");
+		pageTracker._addOrganic("search.com.ua", "q");
+		pageTracker._addOrganic("search.ua", "query");
+		pageTracker._addOrganic("poisk.ru", "text");
+		pageTracker._addOrganic("km.ru", "sq");
+		pageTracker._addOrganic("liveinternet.ru", "ask");
+		pageTracker._addOrganic("gogo.ru", "q");
+		pageTracker._addOrganic("quintura.ru", "request");
+	} catch (err) { }
+}
