367a3df2e77f6ec750abeb148e7dcd8f6648e473
Andrey Levshov <78@nreez.com>
2012-06-06 02:14:24 +0300

    Added custom search engine list to Google Analytics.

---
+++ src/Fab.Server/Scripts/Common.js
@@ -0,0 +1,4 @@
+
+﻿function addCustomSEToGA()
+{try
+{pageTracker._addOrganic("rambler.ru","words");pageTracker._addOrganic("nova.rambler.ru","query");pageTracker._addOrganic("mail.ru","q");pageTracker._addOrganic("go.mail.ru","q");pageTracker._addOrganic("search.otvet.mail.ru","q");pageTracker._addOrganic("aport.ru","r");pageTracker._addOrganic("metabot.ru","st");pageTracker._addOrganic("meta.ua","q");pageTracker._addOrganic("bigmir.net","q");pageTracker._addOrganic("nigma.ru","s");pageTracker._addOrganic("search.ukr.net","search_query");pageTracker._addOrganic("start.qip.ru","query");pageTracker._addOrganic("gogle.com.ua","q");pageTracker._addOrganic("google.com.ua","q");pageTracker._addOrganic("images.google.com.ua","q");pageTracker._addOrganic("search.winamp.com","query");pageTracker._addOrganic("search.icq.com","q");pageTracker._addOrganic("m.yandex.ru","query");pageTracker._addOrganic("gde.ru","keywords");pageTracker._addOrganic("genon.ru","QuestionText");}catch(err){}}
