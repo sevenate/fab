da068215bd5ff2c25c0506badefbddd25352e7d8
Sevenate <1175845+sevenate@users.noreply.github.com>
2012-11-21 02:12:47 +0200

    Updated text for site features; Removed old "login" screenshot.

--- src/Fab.Site/js/init.js
+++ src/Fab.Site/js/init.js
@@ -2,68 +2,55 @@
 /* Orbit Slider */
 $(window).load(function() {
 
-$("#featured").orbit({
-        animation: 'fade',
-        animationSpeed: 800,
-        timer: false,
-        bullets: false,
-        pauseOnHover: true,
-        pauseTime: 1000,
-        fluid: true
-    });
+	$("#featured").orbit({
+			animation: 'fade',
+			animationSpeed: 800,
+			timer: false,
+			bullets: false,
+			pauseOnHover: true,
+			pauseTime: 1000,
+			fluid: true
+		});
 
-var make_button_active = function()
-{
-  //Get item siblings
-  var siblings = $("#header ul li a");
+	var makeButtonActive = function() {
+		$("#header ul li a").each(function(index) {
+			$(this).removeClass('current');
+		});
 
-  //Remove active class on all buttons
-  siblings.each(function (index)
-    {
-      $(this).removeClass('current');
-    }
-  )
+		$(this).addClass('current');
+	};
 
-  //Add the clicked button class
-  $(this).addClass('current');
-}
+	//Attach events to menu
+	$(document).ready(function() {
+		$("#header ul li a").click(makeButtonActive);
 
-//Attach events to menu
-$(document).ready(
-  function()
-  {
-    $("#header ul li a").click(make_button_active);
+		$('#faq h4').each(function() {
+			var tis = $(this), state = false, answer = tis.next('div').slideUp();
+			tis.click(function() {
+				state = !state;
+				answer.slideToggle(state);
+				tis.toggleClass('active', state);
+			});
+		});
 
-    $('#faq h4').each(function() {
-	    var tis = $(this), state = false, answer = tis.next('div').slideUp();
-	    tis.click(function() {
-	      state = !state;
-	      answer.slideToggle(state);
-	      tis.toggleClass('active',state);
-	    });
-	  });
-
-  }  
-)
+	});
 });
 
-
 $(function (){
-
-    $('#header ul li a').each(function(){
-        var path = window.location.href;
-        var current = path.substring(path.lastIndexOf('#'));
-        var url = $(this).attr('href');
-
-        if(url == current){
-            $(this).addClass('current');
-        };
-    });         
+	$('#header ul li a').each(function(){
+		var path = window.location.href;
+		var current = path.substring(path.lastIndexOf('#'));
+		var url = $(this).attr('href');
+		
+		if(url == current){
+			$(this).addClass('current');
+		};
+	});
 });
 
 /* FAQ */
 
 $('.faqlink').click(function(){
-        $('.content').hide();
-        $(this).next('.content').show();
-    });
+	$('.content').hide();
+	$(this).next('.content').show();
+});
