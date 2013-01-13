4e64e0277cd2ae4907c79133f5f163ae50842966
Andrey Levshov <Andrey Levshov@localhost>
2013-01-14 01:42:45 +0200

    - added "debug/release" info to Silverlight start screen - added build configuration info to binary output files property; - updated binary ouput files descriptions on property tab;

--- src/Fab.Server/Properties/AssemblyInfo.cs
+++ src/Fab.Server/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fab.Server")]
-[assembly: AssemblyDescription("FAB WCF Service")]
+[assembly: AssemblyTitle("WCF Service")]
+[assembly: AssemblyDescription("WCF Service")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bff51f58-6e89-4d0b-9695-701029d0d4e3")]
