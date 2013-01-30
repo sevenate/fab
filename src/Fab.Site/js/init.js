5473f446286f43241e023d41af1c820b8ddc6e5b
Andrey Levshov <Andrey Levshov@localhost>
2013-01-30 03:15:58 +0200

    Removed "FAQ" section from site landing page; Added second header on top; Added "Download Silverlight" button; Added "Rss feed" link on the bottom; Rearranged top menu (removed on-page navigation links); Removed jquery.smoothscroll.js and jquery.easing-1.3.pack.js Updated client app screenshot!

--- src/Fab.Site/js/init.js
+++ src/Fab.Site/js/init.js
@@ -23,34 +23,5 @@ $(window).load(function() {
 	//Attach events to menu
 	$(document).ready(function() {
 		$("#header ul li a").click(makeButtonActive);
-
-		$('#faq h4').each(function() {
-			var tis = $(this), state = false, answer = tis.next('div').slideUp();
-			tis.click(function() {
-				state = !state;
-				answer.slideToggle(state);
-				tis.toggleClass('active', state);
-			});
-		});
-
 	});
-});
-
-$(function (){
-	$('#header ul li a').each(function(){
-		var path = window.location.href;
-		var current = path.substring(path.lastIndexOf('#'));
-		var url = $(this).attr('href');
-		
-		if(url == current){
-			$(this).addClass('current');
-		};
-	});
-});
-
-/* FAQ */
-
-$('.faqlink').click(function(){
-	$('.content').hide();
-	$(this).next('.content').show();
 });
